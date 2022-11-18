9ec15afb39b890b52caf148f4dd98969b0a67eb0
Luis Condori <luis2003nb@gmail.com>
2022-11-18 16:25:50 -0300

    Vamos?

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true}).then(() => {
-  server.listen(process.env.PGPORT, () => {
-    console.log(`%s listening at ${process.env.PGPORT}`); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
